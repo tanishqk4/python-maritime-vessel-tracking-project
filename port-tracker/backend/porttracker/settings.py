0d6f8a643cdd6fe6327df5ef3f054fca667b5329
tanishqk4 <tanishq_2312res695@iitp.ac.in>
2025-12-28 22:32:10 +0530

    Fix database config for Render PostgreSQL

--- port-tracker/backend/porttracker/settings.py
+++ port-tracker/backend/porttracker/settings.py
@@ -84,16 +84,27 @@ WSGI_APPLICATION = 'porttracker.wsgi.application'
 # Database (PostgreSQL)
 # ------------------------------------------------------------------------------
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("DB_HOST", "localhost"),
+#         'PORT': os.getenv("DB_PORT", "5432"),
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "5432"),
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # ------------------------------------------------------------------------------
 # Authentication / Custom User Model
